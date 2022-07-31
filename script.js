055b44c747f56faf7dafee55873f4bd45ab67b58
Fonnix <Michaellapointe123@gmail.com>
2022-07-31 13:05:04 -0400

    Add functions for computer and player choice

---
+++ script.js
@@ -0,0 +1,67 @@
+    // ROCK PAPER SCISSORS //
+//
+// COMPUTER CHOICE AND PLAYER CHOICE
+// getComputerChoice function
+// computerSelection = either rock, paper, scissors
+// alert prompt choose rock, paper or scissors
+// playerSelection = either rock, paper, scissors else error
+
+// PLAY A ROUND
+// playRound function
+// if computerSelection == playerSelection draw
+// if computerSelection beats playerSelection computer wins else computer lose
+// return the result of the round
+// 
+// GAME SCORE 
+// game function
+// call playRound to play best of 5
+// keep score throughout the game
+// report the winner or loser at the end
+
+
+// function getComputerChoice() {
+//     const choices = ['Rock', 'Paper', 'Scissors'];
+//     const random = Math.floor(Math.random() * choices.length);
+// }
+// console.log(getComputerChoice(random, choices[random]));
+
+
+
+
+const test = 1;
+console.log(test);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
